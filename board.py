bb0c3f854fed3d9eca70cd9ccb96a2aba3486003
Unknown <camcaswell@gmail.com>
2019-10-07 18:12:16 -0400

    Misc
    added find_paths and manual_build
    various fixes

--- board.py
+++ board.py
@@ -7,6 +7,11 @@ class Cell:
     self.region = None
     # 0-unknown 1-white 2-black
 
+  def __repr__(self):
+    #return f'C[{self.x},{self.y}]-{self.color}-{int(bool(self.region))}'
+    return f'({self.x},{self.y})'
+
+
 class Region:
   def __init__(self, size, color, firstMember):
     self.color = color
@@ -15,11 +20,14 @@ class Region:
     firstMember.region = self
 
 class Board:
-  def __init__(self, grid):
-    self.height = len(grid)
-    self.width = len(grid[0])
+  def __init__(self):
     self.regions = []
     self.cells = {}
+
+
+  def build(self, grid):
+    self.height = len(grid)
+    self.width = len(grid[0])
     for y, row in enumerate(grid):
       for x, val in enumerate(row):
         if val == 0:
@@ -29,6 +37,13 @@ class Board:
           newRegion = Region(val, 1, newCell)
           self.cells[(x,y)] = newCell
           self.regions.append(newRegion)
+
+  def manual_board(self, cell_colors):
+    self.height = len(cell_colors)
+    self.width = len(cell_colors[0])
+    for y, row in enumerate(cell_colors):
+      for x, color in enumerate(row):
+        self.cells[(x,y)] = Cell(x, y, color)
   
   def annex(self, region1, region2):
     region1.members += region2.members
@@ -37,8 +52,9 @@ class Board:
   def _get_list_form(self):
     l = []
     for y in range(self.height):
+      l.append([])
       for x in range(self.width):
-        l[y][x] = self.cells[(x,y)].color
+        l[y].append(self.cells[(x,y)].color)
     return l
   
   def is_solved(self):
@@ -53,10 +69,25 @@ class Board:
                (cell.x, cell.y+1),
                (cell.x, cell.y-1)
               ]
-      for x,y in nbors:
-        if x<0 or x>self.width or y<0 or y>self.height:
-          nbors.remove((x,y))
-      return [self.cells[coords] for coords in nbors]
+      return [self.cells[coords] for coords in nbors if 0<=coords[0]<self.width and 0<=coords[1]<self.height]
+
+  def find_paths(self, start, end, used=[], color=None):
+    if color == None:
+      color = start.color
+    used = used[:]
+    used.append(start)
+    nbors = [nbor for nbor in self.neighbors(start) if nbor not in used and nbor.color in [0, color]]
+    if end in nbors:
+      return [[start, end]]
+    else:
+      used += nbors
+      paths = []
+      for nbor in nbors:
+        for path in self.find_paths(nbor, end, used, color):
+          paths.append([start]+path)
+
+      #return [[start]+path for nbor in nbors for path in self.find_paths(nbor, end, used)]
+      return paths
   
   def __str__(self):
     return str(self._get_list_form())
@@ -64,27 +95,37 @@ class Board:
   # inferences
 
   # put black squares between distinct white regions
-  # test hori and vert pairs
   def create_fences(self, region):
-    for cell in region.members:
-      for nbor in self.neighbors(cell):
-        if nbor.region and nbor.region.color == 1 and nbor.region != region:
+    changeFlag = False
+    # horizontal pairs
+    for x in range(self.width-2):
+      for y in range(self.height-2):
+        cell = self.cells[(x,y)]
+        if cell.region and cell.region.color == 1:
+          potential_fence = self.cells[(x+1,y)]
+          if potentional_fence.color == 0:
+            nbor = self.cells[(x+2,y)]
+            if nbor.region and nbor.region.color == 1 and nbor.region != cell.region:
+              pass
 
 
-def solve(grid):
-  board = Board(grid)
+  def solve(self):
+    while not board.is_solved():
+      # apply inferences
+      pass
 
-  while not board.is_solved():
+
+
   
 
 def main():
   grid = [
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [3, 0, 0, 2, 0],
-        [0, 0, 0, 0, 1],
-        [3, 0, 0, 0, 0]
-      ]
+          [0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0],
+          [3, 0, 0, 2, 0],
+          [0, 0, 0, 0, 1],
+          [3, 0, 0, 0, 0]
+        ]
   
   sol = [
           [2, 2, 2, 2, 2],
@@ -93,14 +134,27 @@ def main():
           [2, 2, 2, 2, 1],
           [1, 1, 1, 2, 2]
         ]
+
+  maze = [
+          [0, 0, 0, 0, 1],
+          [0, 0, 0, 1, 0],
+          [0, 2, 2, 2, 2],
+          [0, 0, 0, 0, 1],
+          [2, 0, 0, 0, 0]
+        ]
   
-  b = solve(grid)
-  print(b)
-  print(b._get_list_form() == sol)
+  b = Board()
+  b.manual_board(maze)
+  cell1 = b.cells[(3,1)]
+  cell2 = b.cells[(4,3)]
+  paths = b.find_paths(cell1, cell2)
+
+  print()
+  for path in paths:
+    print(path)
 
 
 if __name__ == '__main__':
-  c = Cell(1,2,0)
-  c.region = 4
-  print(c.region)
+    main()
+
 
