db38e773d6a2b1172d23face43f5a9c5687328f0
Unknown <camcaswell@gmail.com>
2019-10-20 17:52:59 -0400

    Implemented coordinate labels

--- board_display.py
+++ board_display.py
@@ -5,6 +5,7 @@ WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 400
 WINDOW_BUFFER = 10
 LINE_WIDTH = 2
+COORDS = True
 
 class Board_Display:
     def __init__(self, master, board, title):
@@ -14,29 +15,45 @@ class Board_Display:
         canvas = tk.Canvas(master, width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
         canvas.pack()
 
-        cell_width = (WINDOW_WIDTH-LINE_WIDTH-1-(2*WINDOW_BUFFER))//board.width
-        cell_height = (WINDOW_HEIGHT-LINE_WIDTH-1-(2*WINDOW_BUFFER))//board.height
+        edge_buffer = WINDOW_BUFFER+LINE_WIDTH+1
+        shift = edge_buffer + (20 if COORDS else 0)
 
-        def shift(j):
-            return WINDOW_BUFFER+LINE_WIDTH+1+j
+        cell_width = (WINDOW_WIDTH-(edge_buffer + shift))//board.width
+        cell_height = (WINDOW_HEIGHT-(edge_buffer + shift))//board.height
 
         cmap = {0:'white', 1:'white', 2:'black'}
 
         for cell in board.cells.values():
             canvas.create_rectangle(
-                                    shift(cell.x*cell_width),
-                                    shift(cell.y*cell_height),
-                                    shift((cell.x+1)*cell_width),
-                                    shift((cell.y+1)*cell_height),
+                                    shift + cell.x*cell_width,
+                                    shift + cell.y*cell_height,
+                                    shift + (cell.x+1)*cell_width,
+                                    shift + (cell.y+1)*cell_height,
                                     width=LINE_WIDTH,
                                     fill=cmap[cell.color],
                                 )
             if cell.label is not '':
                 canvas.create_text(
-                                    shift((cell.x+.5)*cell_width),
-                                    shift((cell.y+.5)*cell_height),
+                                    shift + (cell.x+.5)*cell_width,
+                                    shift + (cell.y+.5)*cell_height,
                                     text=cell.label,
                                     font=('Arial', cell_width//3),
+                                )
+
+        if COORDS:
+            for row in range(board.height):
+                canvas.create_text(
+                                    WINDOW_BUFFER+10,
+                                    shift + (row+0.5)*cell_height,
+                                    text=str(row),
+                                    font=('Arial', 10)
+                                )
+            for col in range(board.width):
+                canvas.create_text(
+                                    shift + (col+0.5)*cell_width,
+                                    WINDOW_BUFFER+10,
+                                    text=str(col),
+                                    font=('Arial', 10)
                                 )
 
 
