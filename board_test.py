78acc2e93d591833a9ab0205d4ed08346715c25f
Unknown <camcaswell@gmail.com>
2019-10-21 18:18:01 -0400

    added test_find_reach_white

--- board_test.py
+++ board_test.py
@@ -8,15 +8,15 @@ def BOARD():
 @ pytest.fixture
 def GRID_1():
     b = Board()
-    b.manual_build(
-                    [
-                        [0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0],
-                        [3, 0, 0, 2, 0],
-                        [0, 0, 0, 0, 1],
-                        [3, 0, 0, 0, 0]
-                    ]
-                  )
+    b.build(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [3, 0, 0, 2, 0],
+                    [0, 0, 0, 0, 1],
+                    [3, 0, 0, 0, 0]
+                ]
+              )
     return b
 
 @pytest.fixture
@@ -33,16 +33,37 @@ def SOL_1():
                   )
     return b
 
+@pytest.fixture
+def GRID_2():
+    b = Board()
+    b.build(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 1, 0, 0, 0],
+                    [0, 0, 3, 0, 0],
+                    [0, 0, 0, 0, 0]
+                ]
+            )
+    return b
+
+
 def test_is_solved_true(SOL_1):
-    assert SOL_1.is_solved(), "Test failed"
+    assert SOL_1.is_solved(), "is_solved_true failed"
 
 def test_is_solved_false(GRID_1):
-    assert not GRID_1.is_solved(), "Test failed"
+    assert not GRID_1.is_solved(), "is_solved_false failed"
+
+
+def test_find_reach_white(GRID_2):
+    result = {(1,4), (2,3), (2,4), (3,2), (3,3), (3,4), (4,3)}
+    region = [r for r in GRID_2.white_regions if r.size == 3][0] #the only region of size 3
+    assert {(cell.x,cell.y) for cell in GRID_2.find_reach_white(region, depth_limit=region.size-1)} == result, "find_reach_white failed"
 
 
 def test_find_unreachable(GRID_1, SOL_1):
     GRID_1.find_unreachable()
-    assert GRID_1 == SOL_1, "Test failed"
+    assert GRID_1 == SOL_1, "find_unreachable failed"
 
 
 
